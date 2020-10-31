2e4421288dc7fbe5184107e8c950703b17610d3f
nguyenvankhiem <72650695+nguyenvankhiem@users.noreply.github.com>
2020-10-31 15:22:54 +0700

    Add files via upload

---
+++ bai1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace baithuchanh4
+{
+    class TienDien
+    {
+        protected int sodienthangtruoc, sodienthangnay, sodiendadung;
+        private string tenchuho, diachi;
+        protected int tiendien;
+        public TienDien(int sodienthangtruoc, int sodienthangnay, string tienchuho, string diachi)
+        {
+            this.sodienthangnay = sodienthangnay;
+            this.sodienthangtruoc = sodienthangtruoc;
+            this.tenchuho = tenchuho;
+            this.diachi = diachi;
+        }
+        public TienDien()
+        {
+            sodienthangtruoc = 0;
+            sodienthangnay = 0;
+        }
+        public void Nhap()
+        {
+            Console.Write("Nhap ten chu ho:");
+            tenchuho = Console.ReadLine();
+            Console.Write("Nhap dia chi chu ho:");
+            diachi = Console.ReadLine();
+            Console.Write("Nhap so cong to thang truoc:");
+            sodienthangtruoc = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so cong to thang nay:");
+            sodienthangnay = int.Parse(Console.ReadLine());
+            sodiendadung = sodienthangnay - sodienthangtruoc;
+            Console.WriteLine($"So dien da dung trong thang nay la:{sodiendadung}so");
+        }
+        public void Hien()
+        {
+            Console.SetCursorPosition(0, 6); Console.WriteLine("Ten Chu Ho");
+            Console.SetCursorPosition(tenchuho.Length + 10, 6); Console.WriteLine("Dia Chi");
+            Console.SetCursorPosition(tenchuho.Length + 10 + diachi.Length + 6, 6); Console.WriteLine("So Dien Da Dung");
+            Console.SetCursorPosition(tenchuho.Length + 10 + diachi.Length + 6 + 20, 6); Console.WriteLine("Tien Dien");
+            Console.WriteLine($"{tenchuho}");
+            Console.SetCursorPosition(tenchuho.Length + 10, 7); Console.WriteLine($"{diachi}");
+            Console.SetCursorPosition(tenchuho.Length + 10 + diachi.Length + 6, 7); Console.WriteLine($"{sodiendadung}");
+            Console.SetCursorPosition(tenchuho.Length + 10 + diachi.Length + 6 + 20, 7); Console.WriteLine($"{tiendien}");
+        }
+        public int TinhTienDien()
+        {
+            return sodiendadung * 500;
+        }
+
+    }
+    class TienDienMoi : TienDien
+    {
+        private int dinhmuc;
+        public TienDienMoi(int dinhmuc) : base()
+        {
+            this.dinhmuc = dinhmuc;
+        }
+        public new void Nhap()
+        {
+            base.Nhap();
+            Console.Write("Nhap dinh muc:");
+            dinhmuc = int.Parse(Console.ReadLine());
+        }
+        public void Hien()
+        {
+
+            base.Hien();
+        }
+        public new int TinhTienDien()
+        {
+            if (base.sodiendadung < dinhmuc)
+            {
+                return base.tiendien * 600;
+            }
+            else
+            {
+                return base.tiendien * 500;
+            }
+        }
+
+    }
+    class bai1
+    {
+        static void Main(string[] args)
+        {
+            TienDien a = new TienDien();
+            a.Nhap();
+            a.TinhTienDien();
+            a.Hien();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ bai4.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+namespace bai_4
+{
+    class PTBH
+    {
+        protected int a, b, c;
+        public PTBH()
+        {
+            a = b = c = 0;
+        }
+        public PTBH(int a, int b, int c)
+        {
+            this.a = a; this.b = b; this.c = c;
+        }
+        public void nhap()
+        {
+            Console.WriteLine("nhap he so a"); a = int.Parse(Console.ReadLine());
+            Console.WriteLine("nhap he so b"); b = int.Parse(Console.ReadLine());
+            Console.WriteLine("nhap he so c"); c = int.Parse(Console.ReadLine());
+        }
+        public void hien()
+        {
+            Console.WriteLine("pt {0}x^2+{1}x+{2}=0", a, b, c);
+        }
+        public void giaipt()
+        {
+            int delta = (b * b) - 4 * a * c;
+
+            if (a == 0)
+            {
+                Console.WriteLine("phuong trinh ko phai bac 2");
+            }
+            if (delta == 0)
+            {
+                Console.WriteLine("phuong trinh co nghiem kep" + -b / 2 * a);
+
+            }
+            else if (delta < 0)
+            {
+                Console.WriteLine("pt vo nghiem");
+            }
+            else
+            {
+
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("2 nhiem{0},{1}", x1, x2);
+            }
+        }
+        class test
+        {
+            static void Main(string[] args)
+            {
+                PTBH a = new PTBH();
+                a.nhap(); a.giaipt(); a.hien();
+                Console.ReadLine();
+            }
+        }
+    }
+}
